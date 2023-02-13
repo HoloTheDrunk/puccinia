2c15986c651f5d88282f2b3ec516a6492bd1afbd
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2023-02-13 02:51:47 +0100

    fix: make sure logic thread is joined cleanly

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod frontend;
 mod logic;
 
-use std::sync::mpsc;
+use std::{sync::mpsc, thread::JoinHandle};
 
 use anyhow::bail;
 
@@ -11,22 +11,27 @@ fn main() -> Result<()> {
     let (sender, receiver) = mpsc::channel();
     // frontend::run(receiver):
 
-    std::thread::spawn(move || logic::run(sender));
+    let handler = std::thread::spawn(move || logic::run(sender));
 
     if let Err(err) = frontend::run(receiver) {
         endwin();
+        join_handler(handler)?;
         bail!("{err:?}");
     }
 
-    // if let Err(err) = handler.join() {
-    //     if let Some(err) = err.downcast_ref::<frontend::Error>() {
-    //         return Err(err.clone().into());
-    //     } else {
-    //         bail!("Unhandled frontend error type");
-    //     }
-    // }
-
     endwin();
+
+    Ok(())
+}
+
+fn join_handler<T>(handler: JoinHandle<T>) -> Result<()> {
+    if let Err(err) = handler.join() {
+        if let Some(err) = err.downcast_ref::<frontend::Error>() {
+            return Err(err.clone().into());
+        } else {
+            bail!("Unhandled frontend error type");
+        }
+    }
 
     Ok(())
 }
